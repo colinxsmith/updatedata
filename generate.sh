6fa63a8ee62ddb954ed152b1ea6f4ed62574a94e
Colin Smith <colinxsmith@gmail.com>
2017-11-18 21:44:59 +0000

    add grow method in python

--- generate.sh
+++ generate.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+timelag=18
+
 for stock in $(awk '{for(i=1;i<NF;++i)print $i;}' names)
 do
 	echo $stock
-	./InternetPrice.exe $stock 18 | awk -F, '{print $1,$5}' |sed "/Close/d" > $stock.dat
+	./InternetPrice.exe $stock $timelag | awk -F, '{print $1,$5}' |sed "/Close/d" |sed "/^ $/d"> $stock.dat
 done
 
+python grow.py |awk ' {if($NF == "null") {$NF=$(NF-1);print;}else{print;}} ' | awk ' {for(i=3;i<=NF;++i){if($i == "null") {$i=$(i-1);}else if($i/$(i-1)<0.02){$i*=100;}else if($i/$(i-1)>50){$i*=1e-2;}};print;} '> updated
+
+
+mv update startnew
---
+++ grow.py
@@ -0,0 +1,21 @@
+olddata=open('startnew')
+namesf=open('names')
+names=namesf.readline().split()
+
+while(1):
+	line=olddata.readline()
+	if len(line)==0:break
+	prices=line.split()
+	stock=prices[0]
+	if stock in names:
+		newdat=open(stock+'.dat')
+		linenew=''
+		while(1):
+			newdatl=newdat.readline()
+			if len(newdatl)<=2:break
+			pr=newdatl.split()
+			linenew+=pr[1]+' '
+		print line.strip()+' '+linenew.strip()
+		newdat.close()
+olddata.close()
+			
